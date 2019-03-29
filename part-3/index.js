a9e0734dda7c73a97287f285323a8cb304a2a4ab
Milena Shishkova <milena@a-triks.com>
2019-03-29 20:06:38 +0200

    3

--- part-3/index.js
+++ part-3/index.js
@@ -1,20 +1,15 @@
 'use strict'
 
+let entryArray = ["1", "2", "3", "4", "5", "6", "7", "8"];
+let matrixDictionary = {};
+let matRows = 4;
+let matCols = 4;
+
 var game = (function () {
-    let entryArray = ["1", "2", "3", "4", "5", "6", "7", "8"];
-    let matrixDictionary = {};
-    let matRows = 4;
-    let matCols = 4;
-    // visual
-    let cellClass = '.cell';
 
     function load() {
-        loadJQuery();
-        setTimeout(function () {
-            start();
-            $('.reset-button').on('click', start);
-        }, 500)
-        //start();
+
+        start();
     };
 
     function restart() {
@@ -22,21 +17,27 @@ var game = (function () {
     }
 
     function start() {
-
-        buildMatrix(matRows, matCols);
-        printMatrix(matrixDictionary);
+        consoleApp.buildMatrix(matRows, matCols, entryArray, matrixDictionary);
+        consoleApp.printMatrix(matrixDictionary, matRows, matCols);
     }
 
-    function loadJQuery() {
-        var script = document.createElement('script');
-        script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
-        script.type = 'text/javascript';
-        document.getElementsByTagName('head')[0].appendChild(script);
+    function play(row, col) {
+        consoleApp.play(row, col);
     }
 
+    return {
+        load: load,
+        restart: restart,
+        play: play
+    }
 
-    let newShuffledArraya = shuffle(entryArray);
-    function buildMatrix(rows, cols, ) {
+})();
+
+
+var consoleApp = (function () {
+
+    function buildMatrix(rows, cols, entryArray, matrixDictionary) {
+        let newShuffledArraya = shuffle(entryArray);
         let z = 0;
         for (let i = 0; i < rows; i += 1) {
 
@@ -52,75 +53,15 @@ var game = (function () {
             }
 
         }
-
-        // vizual part
-
-        $('.game-wrapper').html('');
-        for (let i = 0; i < rows; i += 1) {
-            $('<div/>', {
-                id: 'row' + i,
-                class: 'row-' + i,
-                title: 'buu',
-                width: 'auto',
-                height: 'auto'
-            }).appendTo('.game-wrapper');
-
-            for (let j = 0; j < cols; j += 1) {
-
-                let parentClass = '.row-' + i;
-
-                $('<div/>', {
-                        id: 'col' + j,
-                        class: 'closed cell col-' + j,
-                        title: 'buucol',
-                        width: '100px',
-                        height: '100px',
-                    }).css('display', 'inline-block')
-                    .css('border', '2px solid black')
-                    .css('font-size', '40px')
-                    .css('text-align', 'center')
-                    .css('line-height', '100px')
-                    .css('cursor', 'pointer')
-                    .data('state', '')
-                    .html('&nbsp')
-                    .appendTo(parentClass);
-            }
-
-        }
-        $('<div/>', {
-                class: 'btn btn-danger reset-button',
-            }).html('Reset game')
-            .css('width', '200px')
-            .css('margin-top', '20px')
-            .css('border', '2px solid black')
-            .css('font-size', '30px')
-            .css('text-align', 'center')
-            .css('line-height', '100px')
-            .css('cursor', 'pointer')
-            .appendTo('.game-wrapper');
-
-        $('<div/>', {
-                class: 'message-box',
-            }).html('')
-            .css('width', '400px')
-            .css('margin-top', '20px')
-            .css('font-size', '25px')
-            .css('text-align', 'center')
-            .appendTo('.game-wrapper');
-
-            // Populate vizual matrix
-
-            for (let y = 0; y < newShuffledArraya.length; y += 1) {
-                console.log(newShuffledArraya[y]);
-                $($(cellClass)[y]).attr('data-value', newShuffledArraya[y]);
-            }
+        console.log(matrixDictionary);
+        return matrixDictionary;
     }
 
-    function printMatrix(matrix) {
+    function printMatrix(matrixDictionary, matRows, matCols) {
         for (let i = 0; i < matRows; i += 1) {
             let currentRow = '';
             for (let j = 0; j < matCols; j += 1) {
-                let matrixCell = matrix[`row${i}-col${j}`];
+                let matrixCell = matrixDictionary[`row${i}-col${j}`];
                 if (matrixCell.state == 'closed') {
                     currentRow += matrixCell.symbol + ' ';
                 } else if (matrixCell.state == 'opened' || matrixCell.state == 'matched') {
@@ -132,6 +73,7 @@ var game = (function () {
 
         }
         console.log('/********************************************/');
+
     }
 
     function shuffle(array) {
@@ -167,9 +109,10 @@ var game = (function () {
 
     function play(row, col) {
         let selectedCell = matrixDictionary[`row${row}-col${col}`];
+        console.log(selectedCell);
         if (selectedCell) {
             selectedCell.state = 'opened';
-            printMatrix(matrixDictionary);
+            printMatrix( matRows, matrixDictionary);
             let lookForMatch = lookForMatching(row, col);
             if (lookForMatch) {
                 printMatrix(matrixDictionary);
@@ -213,12 +156,15 @@ var game = (function () {
     }
 
     return {
-        load: load,
+        buildMatrix: buildMatrix,
+        printMatrix: printMatrix,
         restart: restart,
         play: play
     }
 
 })();
+
+// Expose functionality
 
 //document.addEventListener("DOMContentLoaded", play);
 
